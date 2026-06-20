f5a0c13dc7d1412259e184500887b51fdcbe3068
blueholding <ahmedmostapha6449@gmail.com>
2026-06-20 16:50:23 +0300

    Update and rename server.js.md to server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const { spawn } = require('child_process');
+
+const child = spawn(
+'node',
+[
+'./node_modules/next/dist/bin/next',
+'start',
+'-p',
+process.env.PORT || '3000'
+],
+{
+stdio: 'inherit'
+}
+);
+
+child.on('close', (code) => {
+process.exit(code);
+});
