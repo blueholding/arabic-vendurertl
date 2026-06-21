93411582e5f11e0da00c58c041e42cf2b1977a69
blueholding <ahmedmostapha6449@gmail.com>
2026-06-21 10:29:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const { spawn } = require('child_process');
 
 const child = spawn(
-'node',
-[
-'./node_modules/next/dist/bin/next',
-'start',
-'-p',
-process.env.PORT || '3000'
-],
-{
-stdio: 'inherit'
-}
+  'node',
+  [
+    './node_modules/next/dist/bin/next',
+    'start',
+    '-p',
+    process.env.PORT || '3000'
+  ],
+  {
+    stdio: 'inherit'
+  }
 );
 
 child.on('close', (code) => {
-process.exit(code);
+  process.exit(code);
 });
